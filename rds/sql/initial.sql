af68de6e7b164386aa4cffc0bf28b20b22c83123
Evgenii Krasnukhin <e.krasnukhin@gmail.com>
2023-01-05 14:08:30 +0300

    fix: change strategy on recreate

--- rds/sql/initial.sql
+++ rds/sql/initial.sql
@@ -1,4 +1,4 @@
-alter database <database-name> collate utf8mb4_bin; -- or utf8_bin on MySQL 5.5.2 or earlier
+alter database <database-name> collate utf8mb4_bin;
 create user <user-name> identified by '<password>';
 grant ALL ON  <database-name>.* to <user-name>;
 grant all privileges on <database-name>.* to <user-name>;
